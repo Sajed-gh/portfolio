c1c338831b6067ec1c6ea2105b6b04d82dfc628b
sajed-gh <mohamedsajed.gharsalli@gmail.com>
2025-12-04 15:41:58 +0100

    css dividing

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
-import '../styles/globals.css'
+import '../styles/base.css';
+import '../styles/layout.css';
+
+import '../styles/components/Navbar.css';
+import '../styles/components/Hero.css';
+import '../styles/components/Lists.css';
+import '../styles/components/Cards.css';
+import '../styles/components/Carousel.css';
+import '../styles/components/Modal.css';
+import '../styles/components/Footer.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
